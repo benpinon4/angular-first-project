bfdc90a181a76d355882bd22486fe2ba893d4f23
benpinon4 <benjamin.pinon@CodeImmersives.com>
2025-02-14 17:22:44 -0500

    {my first commit

---
+++ src/app/components/UserProfile/user-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// user-profile.ts
+@Component({
+    selector: 'user-profile',
+    templateUrl: 'user-profile.html',
+    styleUrl: 'user-profile.css',
+  })
+  export class UserProfile {
+    // Component behavior is defined in here
+  }
